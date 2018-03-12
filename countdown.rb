0fbd9c8aa07a2dd9616d1d039740d682cbc6fdaf
Abel Quintero <abel.j.quintero@gmail.com>
2018-03-12 23:30:28 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(number)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  sleep(5_secs)
+  while number > 0
+    puts "#{number} SECOND(S)"
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
